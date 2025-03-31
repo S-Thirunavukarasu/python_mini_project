0d1a2115f4e8fbe760ec17f43d9cd9eaafc79e71
S-Thirunavukarasu <thirunavukarasus2004@gmail.com>
2025-03-31 19:55:05 +0530

    Guess_Number

--- Guess_Number.py
+++ Guess_Number.py
@@ -2,7 +2,14 @@ import random
 
 def guess_number():
     while True:
-        user_choice=int(input("Enter a number from 1 to 10 : "))
+        try:
+            user_choice=int(input("Enter a number from 1 to 10 : "))
+        except ValueError:
+            print("Enter a valid number.")
+            continue
+        if user_choice<0 or user_choice>10:
+            print("Enter a number between 1 to 10")
+            continue
         comp_choice=random.randint(1,10)
         if user_choice==comp_choice:
             print(f"You Won!.You and computer have chosen {comp_choice}")
